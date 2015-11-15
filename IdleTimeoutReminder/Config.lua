1a5759106aa2399cd01005fbd6d182c55a57f96f
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2015-11-15 21:30:23 +0100

    Comment improvements

--- IdleTimeoutReminder/Config.lua
+++ IdleTimeoutReminder/Config.lua
@@ -7,11 +7,11 @@ https://github.com/CaptainBlagbird
 --]]
 
 IdleTimeoutReminder = {
-	-- How long to wait between checking for idle state (in seconds)
+	-- How long to wait between checking for idle state change (in seconds)
 	CYCLE_TIME_S = 20,
 	-- How long to wait until the notification is displayed after player went idle (in minutes)
 	-- (has to be larger than DATA_UPDATE_DELAY_MS)
 	NOTIFICATION_TIME_M = 14,
-	-- Message to display in notification
+	-- Message to display in the notification
 	MESSAGE = "",
 }
